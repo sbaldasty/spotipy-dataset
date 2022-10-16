23a7b7ba5f52d6255ce90fe940535aa312ec0c55
Steven Baldasty <sbaldasty@gmail.com>
2022-10-16 12:46:46 -0400

    Add audio analysis and documentation

---
+++ README.md
@@ -0,0 +1,49 @@
+The columns are
+1. Track Id
+2. explicit
+3. acousticness
+4. danceability
+5. duration_ms
+6. energy
+7. instrumentalness
+8. key
+9. liveness
+10. loudness
+11. mode
+12. speechiness
+13. tempo
+14. time_signature
+15. valence
+16. bar count
+17. mean bar duration
+18. std dev bar duration
+19. mean bar confidence
+20. std dev bar confidence
+21. beat count
+22. mean beat duration
+23. std dev beat duration
+24. mean bar confidence
+25. std dev bar confidence
+26. section count
+27. mean section duration
+28. std dev section duration
+29. mean section confidence
+30. std dev section confidence
+31. mean section loudness
+32. std dev section loudness
+33. mean section tempo
+34. std dev section tempo
+35. section key count
+36. mean section key confidence
+37. std dev section key confidence
+38. section mode count
+39. mean section mode confidence
+40. std dev section mode confidence
+41. section time signature count
+42. mean section time signature confidence
+43. std dev time signature confidence
+44. tatum count
+45. mean tatum duration
+46. std dev tatum duration
+47. mean tatum confidence
+48. std dev tatum confidence
--- fetch.py
+++ fetch.py
@@ -3,8 +3,14 @@ import os
 import spotipy
 import traceback
 
+from math import sqrt
 from pathlib import Path
 from spotipy.oauth2 import SpotifyClientCredentials
+
+spotify_client_id = os.environ['SPOTIFY_CLIENT_ID']
+spotify_client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+spotify_credentials = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
+spotify_client = spotipy.Spotify(auth_manager=spotify_credentials)
 
 def ensure_file_exists(filename):
     Path(filename).touch(exist_ok=True)
@@ -19,10 +25,86 @@ def save_list(filename, list):
         for element in list:
             file.write(element + '\n')
 
+def count_distinct_values(list, property):
+    values = set()
+    for element in list:
+        values.add(element[property])
+    return len(values)
+
+def mean(list, property):
+    sum = 0
+    count = len(list)
+    for element in list:
+        sum += element[property]
+    return 0 if count == 0 else sum / count
+
+def standard_deviation(list, property):
+    sum = 0
+    count = len(list)
+    avg = mean(list, property)
+    for element in list:
+        sum += (element[property] - avg) ** 2
+    return 0 if count == 0 else sqrt(sum / count)
+
+def append_audio_features(row):
+    audio_feature = spotify_client.audio_features([row[0]])[0]
+    row.append(audio_feature['acousticness']) # Column 3
+    row.append(audio_feature['danceability']) # Column 4
+    row.append(audio_feature['duration_ms']) # Column 5
+    row.append(audio_feature['energy']) # Column 6
+    row.append(audio_feature['instrumentalness']) # Column 7
+    row.append(audio_feature['key']) # Column 8
+    row.append(audio_feature['liveness']) # Column 9
+    row.append(audio_feature['loudness']) # Column 10
+    row.append(audio_feature['mode']) # Column 11
+    row.append(audio_feature['speechiness']) # Column 12
+    row.append(audio_feature['tempo']) # Column 13
+    row.append(audio_feature['time_signature']) # Column 14
+    row.append(audio_feature['valence']) # Column 15
+
 def append_audio_analysis(row):
-    # TODO Actually append audio analysis...
     audio_analysis = spotify_client.audio_analysis(row[0])
-    row.append(audio_analysis[''])
+
+    # Bars
+    row.append(len(audio_analysis['bars'])) # Column 16
+    row.append(mean(audio_analysis['bars'], 'duration')) # Column 17
+    row.append(standard_deviation(audio_analysis['bars'], 'duration')) # Column 18
+    row.append(mean(audio_analysis['bars'], 'confidence')) # Column 19
+    row.append(standard_deviation(audio_analysis['bars'], 'confidence')) # Column 20
+
+    # Beats
+    row.append(len(audio_analysis['beats'])) # Column 21
+    row.append(mean(audio_analysis['beats'], 'duration')) # Column 22
+    row.append(standard_deviation(audio_analysis['beats'], 'duration')) # Column 23
+    row.append(mean(audio_analysis['beats'], 'confidence')) # Column 24
+    row.append(standard_deviation(audio_analysis['beats'], 'confidence')) # Column 25
+
+    # Sections
+    row.append(len(audio_analysis['sections'])) # Column 26
+    row.append(mean(audio_analysis['sections'], 'duration')) # Column 27
+    row.append(standard_deviation(audio_analysis['sections'], 'duration')) # Column 28
+    row.append(mean(audio_analysis['sections'], 'confidence')) # Column 29
+    row.append(standard_deviation(audio_analysis['sections'], 'confidence')) # Column 30
+    row.append(mean(audio_analysis['sections'], 'loudness')) # Column 31
+    row.append(standard_deviation(audio_analysis['sections'], 'loudness')) # Column 32
+    row.append(mean(audio_analysis['sections'], 'tempo')) # Column 33
+    row.append(standard_deviation(audio_analysis['sections'], 'tempo')) # Column 34
+    row.append(count_distinct_values(audio_analysis['sections'], 'key')) # Column 35
+    row.append(mean(audio_analysis['sections'], 'key_confidence')) # Column 36
+    row.append(standard_deviation(audio_analysis['sections'], 'key_confidence')) # Column 37
+    row.append(count_distinct_values(audio_analysis['sections'], 'mode')) # Column 38
+    row.append(mean(audio_analysis['sections'], 'mode_confidence')) # Column 39
+    row.append(standard_deviation(audio_analysis['sections'], 'mode_confidence')) # Column 40
+    row.append(count_distinct_values(audio_analysis['sections'], 'time_signature')) # Column 41
+    row.append(mean(audio_analysis['sections'], 'time_signature_confidence')) # Column 42
+    row.append(standard_deviation(audio_analysis['sections'], 'time_signature_confidence')) # Column 43
+
+    # Tatums
+    row.append(len(audio_analysis['tatums'])) # Column 44
+    row.append(mean(audio_analysis['tatums'], 'duration')) # Column 45
+    row.append(standard_deviation(audio_analysis['tatums'], 'duration')) # Column 46
+    row.append(mean(audio_analysis['tatums'], 'confidence')) # Column 47
+    row.append(standard_deviation(audio_analysis['tatums'], 'confidence')) # Column 48
 
 class SearchQueue:
 
@@ -96,49 +178,29 @@ class SpotifySearch:
         self.album_ids.save()
         self.track_ids.save()
 
-spotify_client_id = os.environ['SPOTIFY_CLIENT_ID']
-spotify_client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
-spotify_credentials = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
-spotify_client = spotipy.Spotify(auth_manager=spotify_credentials)
-
 output_file = 'output.csv'
 csv_file = open(output_file, 'a+')
 csv_writer = csv.writer(csv_file)
 search = SpotifySearch(spotify_client)
 
 if os.path.getsize(output_file) > 0:
-    # Pick up where we left off
+    print('File ' + output_file + ' exists, attempting to continue data gathering')
     search.load()
 else:
-    # Or start fresh seeding with any old artist
+    print('No saved progress, starting fresh with any old artist')
     search.artist_ids.add('0TnOYISbd1XYRBk9myaseg')
 
 try:
     for i in range(30):
         track_id = search.get_next_track()
         track = spotify_client.track(track_id)
-        audio_feature = spotify_client.audio_features([track_id])[0]
-        row = []
-
-        # Just a sanity check
         print('[ARTIST] ', track['artists'][0]['name'], ' [ALBUM] ', track['album']['name'], ' [TRACK] ', track['name'])
 
-        row.append(track_id)
-        row.append(track['explicit'])
-        row.append(audio_feature['acousticness'])
-        row.append(audio_feature['danceability'])
-        row.append(audio_feature['duration_ms'])
-        row.append(audio_feature['energy'])
-        row.append(audio_feature['instrumentalness'])
-        row.append(audio_feature['key'])
-        row.append(audio_feature['liveness'])
-        row.append(audio_feature['loudness'])
-        row.append(audio_feature['mode'])
-        row.append(audio_feature['speechiness'])
-        row.append(audio_feature['tempo'])
-        row.append(audio_feature['time_signature'])
-        row.append(audio_feature['valence'])
-        #append_audio_analysis(row)
+        row = []
+        row.append(track_id) # Column 1
+        row.append(track['explicit']) # Column 2
+        append_audio_features(row)
+        append_audio_analysis(row)
         csv_writer.writerow(row)
 
 except:
