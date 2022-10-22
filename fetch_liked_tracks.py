361410fd5eb34d3fcd9a9003db0bb8b065364298
Steven Baldasty <sbaldasty@gmail.com>
2022-10-22 16:06:52 -0400

    Fix limit

--- fetch_liked_tracks.py
+++ fetch_liked_tracks.py
@@ -20,7 +20,7 @@ while True:
 
     # I guess this is supposed to be a proxy for Liked songs? I don't think it's exactly what we want!
     # https://community.spotify.com/t5/Spotify-for-Developers/Expose-liked-songs-playlist-to-3rd-party-API-Sonos/td-p/4959924
-    saved_tracks = spotify_client.current_user_saved_tracks(limit=2, offset=offset)
+    saved_tracks = spotify_client.current_user_saved_tracks(limit=30, offset=offset)
 
     if len(saved_tracks['items']) == 0:
         break
