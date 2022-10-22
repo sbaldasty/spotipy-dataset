5dde81ffb3a1ec3e58d372aed42e5dca459d3f85
Steven Baldasty <sbaldasty@gmail.com>
2022-10-22 16:05:08 -0400

    Add pagination

--- fetch_liked_tracks.py
+++ fetch_liked_tracks.py
@@ -1,5 +1,4 @@
 # TODO Why do I need to supply a username before asking the user to authenticate?!
-# TODO Page through the liked songs so we can collect all of them
 # TODO Looks like we can only get "saved" songs, which are not sctrictly "liked" songs; at least make terminology consistent
 
 import csv
@@ -7,12 +6,6 @@ import spotipy
 import spotipy.util as util
 
 from shared import append_track
-
-def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
 
 username = '12100797839' # Hardcoded to me, for now
 token = util.prompt_for_user_token(username, redirect_uri='http://localhost:8888/spotifycallback', scope='user-library-read')
@@ -22,13 +15,16 @@ output_file = 'saved_tracks_' + username + ".csv"
 csv_file = open(output_file, 'a+')
 csv_writer = csv.writer(csv_file)
 
-# I guess this is supposed to be a proxy for Liked songs? I don't think it's exactly what we want!
-# https://community.spotify.com/t5/Spotify-for-Developers/Expose-liked-songs-playlist-to-3rd-party-API-Sonos/td-p/4959924
-saved_tracks = spotify_client.current_user_saved_tracks(limit=30)
+offset = 0
+while True:
 
-track_ids = []
-for item in saved_tracks['items']:
-    track_ids.append(item['track']['id'])
+    # I guess this is supposed to be a proxy for Liked songs? I don't think it's exactly what we want!
+    # https://community.spotify.com/t5/Spotify-for-Developers/Expose-liked-songs-playlist-to-3rd-party-API-Sonos/td-p/4959924
+    saved_tracks = spotify_client.current_user_saved_tracks(limit=2, offset=offset)
 
-for track_id in track_ids:
-    append_track(spotify_client, csv_writer, track_id)
+    if len(saved_tracks['items']) == 0:
+        break
+
+    for item in saved_tracks['items']:
+        append_track(spotify_client, csv_writer, item['track']['id'])
+        offset += 1
