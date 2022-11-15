1fc60045ee65fd948eae47312ee4642c56d41c76
Steven Baldasty <sbaldasty@gmail.com>
2022-11-14 22:30:56 -0500

    Misc improvements

--- fetch_saved_tracks.py
+++ fetch_saved_tracks.py
@@ -3,18 +3,21 @@
 import csv
 import spotipy
 import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
 
 from shared import append_track
 
-username = '12100797839' # Hardcoded to me, for now
+username = 'doublelock' # Hardcoded to me, for now
 token = util.prompt_for_user_token(username, redirect_uri='http://localhost:8888/spotifycallback', scope='user-library-read')
 spotify_client = spotipy.Spotify(auth=token)
 
 output_file = 'saved_tracks_' + username + ".csv"
-csv_file = open(output_file, 'a+')
+csv_file = open(output_file, 'a+', encoding='utf-8', newline="")
 csv_writer = csv.writer(csv_file)
 
 offset = 0
+trackCounter = 0
+
 while True:
 
     # I guess this is supposed to be a proxy for Liked songs? I don't think it's exactly what we want!
@@ -25,5 +28,16 @@ while True:
         break
 
     for item in saved_tracks['items']:
-        append_track(spotify_client, csv_writer, item['track']['id'])
-        offset += 1
+        try:
+            append_track(spotify_client, csv_writer, item['track']['id'])
+            offset += 1
+            trackCounter += 1
+            if trackCounter % 10 == 0:
+                print("Track Count: " + str(trackCounter))
+
+        except KeyboardInterrupt:
+            break
+
+        except:
+            print("ERROR: No audio features")
+            offset += 1
